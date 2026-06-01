9a0eef56fb939c6d02fdfcf28247fdf85fb544be
Vinycius Lu <vinyciuslu@gmail.com>
2026-06-01 13:49:56 -0300

    feat: iniciando a implementação do dashboard

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const content = document.getElementById("content");
+
+function updateClock() {
+
+    const now = new Date();
+
+    document.getElementById("clock").innerHTML =
+        now.toLocaleDateString("pt-BR") +
+        " " +
+        now.toLocaleTimeString("pt-BR");
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
